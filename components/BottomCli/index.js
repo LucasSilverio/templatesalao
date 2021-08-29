709a761d2f22ef796d2cd3e1fb08f10065f24a12
lucas <lucas.silveriosousa@gmail.com>
2021-08-29 18:46:13 -0300

    Ajuste Formulário nova conta

--- components/BottomCli/index.js
+++ components/BottomCli/index.js
@@ -69,7 +69,7 @@ class BottomCli extends Component {
         // boxView.style.display = 'none';
         // Show the prompt
         deferredPrompt.prompt();
-        // Wait for the user to respond to the prompt
+        // Wait for the user to respond to the prompt 
         deferredPrompt.userChoice.then((choiceResult) => {
           if (choiceResult.outcome === 'accepted') {
             console.log('User accepted the A2HS prompt');
