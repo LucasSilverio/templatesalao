24ccf109af8f2911ffd231371d501d37c9824efc
lucas <lucas.silveriosousa@gmail.com>
2021-08-29 20:01:29 -0300

    Teste pwa

--- components/BottomCli/index.js
+++ components/BottomCli/index.js
@@ -49,38 +49,38 @@ class BottomCli extends Component {
   }
 
   componentDidMount(){
-    let deferredPrompt;
-    const addBtn = document.querySelector('.add-button');
-    const boxView = document.querySelector('.box-view');
-    addBtn.style.display = 'none';
-    // boxView.style.display = 'none';
-    window.addEventListener('beforeinstallprompt', (e) => {
-      // Prevent Chrome 67 and earlier from automatically showing the prompt
-      e.preventDefault();
-      // Stash the event so it can be triggered later.
-      deferredPrompt = e;
-      // Update UI to notify the user they can add to home screen
-      addBtn.style.display = 'block';
-      // boxView.style.display = 'block';
-      console.log('xxx')
-      addBtn.addEventListener('click', () => {
-        console.log('clicou')
-        // hide our user interface that shows our A2HS button
-        addBtn.style.display = 'none';
-        // boxView.style.display = 'none';
-        // Show the prompt
-        deferredPrompt.prompt();
-        // Wait for the user to respond to the prompt 
-        deferredPrompt.userChoice.then((choiceResult) => {
-          if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the A2HS prompt');
-          } else {
-            console.log('User dismissed the A2HS prompt');
-          }
-          deferredPrompt = null;
-        });
-      });
-    });
+    // let deferredPrompt;
+    // const addBtn = document.querySelector('.add-button');
+    // const boxView = document.querySelector('.box-view');
+    // addBtn.style.display = 'none';
+    // // boxView.style.display = 'none';
+    // window.addEventListener('beforeinstallprompt', (e) => {
+    //   // Prevent Chrome 67 and earlier from automatically showing the prompt
+    //   e.preventDefault();
+    //   // Stash the event so it can be triggered later.
+    //   deferredPrompt = e;
+    //   // Update UI to notify the user they can add to home screen
+    //   addBtn.style.display = 'block';
+    //   // boxView.style.display = 'block';
+    //   console.log('xxx')
+    //   addBtn.addEventListener('click', () => {
+    //     console.log('clicou')
+    //     // hide our user interface that shows our A2HS button
+    //     addBtn.style.display = 'none';
+    //     // boxView.style.display = 'none';
+    //     // Show the prompt
+    //     deferredPrompt.prompt();
+    //     // Wait for the user to respond to the prompt 
+    //     deferredPrompt.userChoice.then((choiceResult) => {
+    //       if (choiceResult.outcome === 'accepted') {
+    //         console.log('User accepted the A2HS prompt');
+    //       } else {
+    //         console.log('User dismissed the A2HS prompt');
+    //       }
+    //       deferredPrompt = null;
+    //     });
+    //   });
+    // });
   }
 
   installApp() {
@@ -124,7 +124,7 @@ class BottomCli extends Component {
             </Opcoes> */}
           </AdicionarTelaInicial>
           <Opcoes >
-              <button class="add-button" onClick={this.installApp}>Adicione Nosso App à Sua Tela Inicial</button>
+              {/* <button class="add-button" onClick={this.installApp}>Adicione Nosso App à Sua Tela Inicial</button> */}
           </Opcoes>
       </Container>
     )
