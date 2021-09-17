3d0479094620c140fb380f01bef6975b48ee6524
lucas <lucas.silveriosousa@gmail.com>
2021-09-17 16:34:12 -0300

    Tag Analitycs

--- pages/index.js
+++ pages/index.js
@@ -53,6 +53,20 @@ class Home extends Component {
     return(
       <>
         <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-140120435-7"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'UA-140120435-7');
+                `,
+            }}
+          />
           <meta charset='utf-8' />
           <meta http-equiv='X-UA-Compatible' content='IE=edge' />
           <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
